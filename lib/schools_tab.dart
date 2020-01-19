cbd4f30c9f848b18be6d3cef3b5fcf6ad0def82e
hecris <christopher.he33@myhunter.cuny.edu>
2020-01-18 22:58:48 -0500

    schools tab: add sliding drawer
    use spaces instead of tabs

--- lib/schools_tab.dart
+++ lib/schools_tab.dart
@@ -2,72 +2,93 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class School {
-	String school_name;
-	LatLng latlng;
-	
-	School(String name, num lat, num long) {
-		this.school_name = name;
-		this.latlng = LatLng(lat, long);
-	}
+    String school_name;
+    LatLng latlng;
 
-	LatLng get position {
-		return latlng;
-	}
+    School(String name, num lat, num long) {
+        this.school_name = name;
+        this.latlng = LatLng(lat, long);
+    }
 
-	String get name {
-		return school_name;
-	}
+    LatLng get position {
+        return latlng;
+    }
+
+    String get name {
+        return school_name;
+    }
 }
 
 class SchoolsTab extends StatefulWidget {
-	@override
-	_SchoolsTabState createState() => _SchoolsTabState();
+    @override
+    _SchoolsTabState createState() => _SchoolsTabState();
 }
 
 class _SchoolsTabState extends State<SchoolsTab> {
-	GoogleMapController mapController;
+    GoogleMapController mapController;
 
-	// final LatLng _center = const LatLng(45.521563, -122.677433);
-	final LatLng _center = const LatLng(40.7128, -74.0060);
-	final Map<String, Marker> _markers = {};
+    final LatLng _center = const LatLng(40.7128, -74.0060);
+    final Map<String, Marker> _markers = {};
+    GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
+    School _current_school = new School('', 0.0, 0.0);
 
-	Future<void> _onMapCreated(GoogleMapController controller) async {
-		setState(() {
-			mapController = controller;
-			num num_schools = 2;
-			var schools = new List(num_schools);
-			schools[0] = School("Stuyvesant High School", 40.7178801, -74.0137509);
-			schools[1] = School("The Bronx High School of Science", 40.87833, -73.89083);
+    Future<void> _onMapCreated(GoogleMapController controller) async {
+        setState(() {
+            mapController = controller;
+            num num_schools = 2;
+            var schools = new List(num_schools);
+            schools[0] = School("Stuyvesant High School", 40.7178801, -74.0137509);
+            schools[1] = School("The Bronx High School of Science", 40.87833, -73.89083);
 
-			for (final school in schools) {
-				final marker = Marker(
-						markerId: MarkerId(school.name),
-						position: school.position,
-						// TODO: Should open a sliding panel
-						onTap: openSlidingPanel,
-				);
-				_markers[school.name] = marker;
-			}
-		});
+            for (final school in schools) {
+                final marker = Marker(
+                        markerId: MarkerId(school.name),
+                        position: school.position,
+                        onTap: () => showSchoolInfo(school),
+                );
+                _markers[school.name] = marker;
+            }
+        });
 
-	}
+    }
 
-	void openSlidingPanel() {
-		print("hello");
-	}
+    void showSchoolInfo(School school) {
+        setState(() {
+            _current_school = school;
+            _drawerKey.currentState.openDrawer();
+        }
+        );
+    }
 
-	@override
-	Widget build(BuildContext context) {
-		return Container(
-				child: GoogleMap(
-						onMapCreated: _onMapCreated,
-						initialCameraPosition: CameraPosition(
-								target: _center,
-								zoom: 10.0,
-						),
-						markers: _markers.values.toSet(),
-				)
-
-		);
-	}
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+                key: _drawerKey,
+                body: Center(
+                        child: GoogleMap(
+                                onMapCreated: _onMapCreated,
+                                initialCameraPosition: CameraPosition(
+                                        target: _center,
+                                        zoom: 10.0,
+                                ),
+                                markers: _markers.values.toSet(),
+                        )
+                ),
+                // disables user from opening the drawer themselves, since
+                // the drawer should only open when a marker is tapped
+                drawerEdgeDragWidth: 0.0,
+                drawer: Drawer(
+                        child: ListView(
+                                children: <Widget> [
+                                    DrawerHeader (
+                                            child: Text(_current_school.name),
+                                            decoration: BoxDecoration(
+                                                    color: Colors.blue,
+                                            ),
+                                    ),
+                                ]
+                        )
+                ),
+                );
+    }
 }
